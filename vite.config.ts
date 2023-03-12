b19cef12017a8a622b51deb61ae08cadebeed2f0
anncer <anncer@163.com>
2023-03-12 22:28:02 +0800

    build Project

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import DefineOptions from "unplugin-vue-define-options/vite";
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    DefineOptions(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+    })
+  ],
+})
